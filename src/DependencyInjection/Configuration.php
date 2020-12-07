244b567996540d8945dc0a62b712ba8751436c0e
Viktor Nazrov <v.nazarov@bitms.ru>
2020-12-07 17:42:10 +0300

    v3.1.6

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -10,18 +10,14 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder() : TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('bitms');
+        $treeBuilder = new TreeBuilder('consul');
 
         $treeBuilder->getRootNode()
             ->children()
-                ->arrayNode('consul')
-                    ->children()
                         ->scalarNode('serverName')->end()
                         ->scalarNode('hostName')->end()
                         ->scalarNode('port')->end()
                         ->scalarNode('ttl')->end()
-                    ->end()
-                ->end()
             ->end()
         ;
 
