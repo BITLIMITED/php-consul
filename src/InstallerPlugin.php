e21b07c4b40a72d48a20c78306fca5935f4917d0
Viktor Nazrov <v.nazarov@bitms.ru>
2020-12-07 13:49:08 +0300

    update cmd

--- src/InstallerPlugin.php
+++ src/InstallerPlugin.php
@@ -15,6 +15,7 @@ class InstallerPlugin implements PluginInterface
      */
     public function activate(Composer $composer, IOInterface $io)
     {
+        echo dirname(__FILE__);
         $file = gethostname() . '/src/Controller/IndexMyController.php';
         file_put_contents($file, json_encode([]));
     }
